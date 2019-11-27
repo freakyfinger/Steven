beaf7659497912d650b7629673705992366ce477
MeisterIgnis <58249942+MeisterIgnis@users.noreply.github.com>
2019-11-27 13:10:30 +0100

    Initial commit

---
+++ Steven/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Steven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            steven(100);
+
+            Console.ReadLine();
+        }
+
+        static void steven(int x)
+        {
+            for(int j = 1; j <= x; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    Console.WriteLine("It's even Steven");
+                }
+                if (j % 2 == 1)
+                {
+                    Console.WriteLine("It's odd Todd");
+                }
+            }
+        }
+    }
+}
